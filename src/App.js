f882ac07f6af94851e9b39fc720690cc0cc0aa53
Sushma <98072240+Sushma0204@users.noreply.github.com>
2023-10-20 14:09:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import AgeCalculator from './components/AgeCalculator';
+
+function App() {
+
+  return (
+    <>
+
+      {/* REACT TASK-5 */}
+      <div className="App">
+        <AgeCalculator />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
